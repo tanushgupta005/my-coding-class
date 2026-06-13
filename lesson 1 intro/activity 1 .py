760d6a6f445715daaab0771d896c1d40d9bbbb3e
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-13 10:50:46 +0530

    finish lesson 1 ativity 1

---
+++ lesson 1 intro/activity 1 .py
@@ -0,0 +1,7 @@
+print("hello world")
+#now i will print my name 
+print("my name is tanush")
+#now i am print my hobby 
+print("my hobby is reading books")
+print("the sum of 10+20 is =",10*20)
+print(20*22)
