989f04352c7269f6c587efb25ebaacfd8d4b64b6
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-18 10:42:39 +0530

    finished l3a1

---
+++ lesson 3/activtty1.py
@@ -0,0 +1,16 @@
+# 1) Store an integer value in `a`.
+a=123
+# 2) Print the datatype of `a` using `type()`.
+print("the data type of a is =",type(a))
+# 3) Store a decimal (float) value in `b`.
+b=12.2
+# 4) Print the datatype of `b` using `type()`.
+print("the data type of b is=",type(b))
+# 5) Store a text (string) value in `c`.
+c="hello"
+# 6) Print the datatype of `c` using `type()`.
+print("the data type of c is=",type(c))
+# 7) Store a boolean value (True/False) in `d`.
+d=1<5
+# 8) Print the datatype of `d` using `type()`.
+print("the data type of d is=",type(d))
