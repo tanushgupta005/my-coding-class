05af860e5cb2668e4a4f220b09b428f0c19c3617
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-18 11:06:29 +0530

    finish l3a2

---
+++ lesson 3/activity2.py
@@ -0,0 +1,11 @@
+x=1.23456
+print("BEFORE type-casting=",x,type(x))
+
+x=int(x)
+print("after type casting=",x,type(x) )
+
+x=-15
+print("BEFORE type-casting=",x,type(x))
+
+x=bool(x)
+print("after type casting=",x,type(x) )
