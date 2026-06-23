1775e0bfa6f6ac4c0b74f96ba1d9722d10d5fcbb
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-23 10:58:27 +0530

    finish l5 a4

---
+++ lesson 5 if conditions/activity 4.py
@@ -0,0 +1,8 @@
+# find if number is even or odd 
+num1=int(input("enter a number"))
+if num1%2==1:
+    print('NUMBER IS ODD')
+else:
+    print("number is even")
+#if num1%2==0:
+# print("NUMBER IS EVEN")
