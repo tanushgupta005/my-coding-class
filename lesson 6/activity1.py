bae0271c49a9e168876bb5b268d7df5e1f291680
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-24 10:49:22 +0530

    finish a1 l6

---
+++ lesson 6/activity1.py
@@ -0,0 +1,14 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+if b>0 and a>0:
+    print("both numbers are positive")
+else:
+    print("one or both are negitive")
+
+if a>0 or b>0:
+    print("at least 1 is positive ")
+else:
+    print("BOTH ARE NEGITIVE")
+
+if not a==0:
+    print ("a is not zero")
