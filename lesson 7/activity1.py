672b2fef52da18fe2cd0c91098969730c431c658
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-25 11:10:19 +0530

    finish a1 l7

---
+++ lesson 7/activity1.py
@@ -0,0 +1,8 @@
+tanush_friend=["rohit"]
+sir_friend=["rohit"]
+if tanush_friend==sir_friend:
+    print("both are same friends")
+if tanush_friend is sir_friend:
+    print("both have same identiy")
+else:
+    print("both have diffrent identity")
