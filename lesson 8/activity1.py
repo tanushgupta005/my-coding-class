a08fea6574514840712eb118e8f61214ea1deee4
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-27 11:58:51 +0530

    finish a1l8

---
+++ lesson 8/activity1.py
@@ -0,0 +1,26 @@
+
+# 1) Store values in `v`, `w`, `x`, `y`, and `z`.
+v=78
+w=34
+x=89
+y=90
+# 2) Calculate the expression (v + w) * x / y and store the result back in `z`.
+z=(v+w)*x/y
+# 3) Print the value of `z` with a message.
+print("the value of Z=",z)
+# 4) Store a name in `name` and a number in `age`.
+name="tanush"
+age=56
+# 5) Check this condition using `or` and `and`:
+if (name=="alex" or name=="john") and age>=2: 
+    print("welcome")
+else:
+    print("goodbye")
+
+# - The code checks if `name` is "Alex"
+
+# OR (if `name` is "John" AND `age` is 2 or more).
+
+# - If the condition is true, print the welcome message.
+
+# - Otherwise, print the goodbye message.
