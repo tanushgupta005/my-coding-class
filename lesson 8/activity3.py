1d3a1dd178ae30564f6db777f05555a8108d4d2a
Tanush Gupta <tanushgupta005@gmail.com>
2026-06-27 12:27:18 +0530

    finisha3 l8

---
+++ lesson 8/activity3.py
@@ -0,0 +1,33 @@
+# 1) Store the given values:
+wrongmean=38
+correctnum=56
+incorrectnum=36
+totalnum=400
+# `mean1` (wrong mean), `wrong_number`, `correct_number`, and `total_number`.
+sum=wrongmean*totalnum
+correctmean=(sum-incorrectnum+correctnum)/totalnum
+# 2) Calculate the total sum using the wrong mean:
+print(correctmean)
+# - Multiply `mean1` by `total_number`
+
+# - Store it in `sum`
+
+# - Print the sum.
+
+# 3) Fix the sum to get the correct total:
+
+# - Remove the wrong number (subtract `wrong_number`)
+
+# - Add the correct number (add `correct_number`)
+
+# - Store the corrected total in `num2`
+
+# - Print the corrected sum.
+
+# 4) Find the correct mean:
+
+# - Divide `num2` by `total_number`
+
+# - Store it in `mean2`
+
+# - Print `mean2`.
